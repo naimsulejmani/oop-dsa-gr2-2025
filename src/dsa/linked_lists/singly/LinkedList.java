d91a9b2445c286e85abe24b10ca55b460da9d19d
Naim Sulejmani <naim.sulejmani@gmail.com>
2025-04-22 20:12:28 +0200

    Linked list adding feature

--- src/dsa/linked_lists/singly/LinkedList.java
+++ src/dsa/linked_lists/singly/LinkedList.java
@@ -2,19 +2,21 @@ package dsa.linked_lists.singly;
 
 public class LinkedList {
     private Node head;
+    private int size;
 
     public LinkedList() {
 
     }
 
     public boolean isEmpty() {
-        return head == null;
+        return head == null; // return size == 0;
     }
 
     public void addFirst(int data) {
         Node newNode = new Node(data);
         newNode.setNext(head);
         head = newNode;
+        size++;
     }
 
 
@@ -32,6 +34,7 @@ public class LinkedList {
             return;
         }
         head = head.getNext();
+        size--;
     }
 
 
@@ -53,6 +56,7 @@ public class LinkedList {
         }
         // Set the final node's next reference to the new node, effectively adding it to the end
         temp.setNext(newNode);
+        size++;
     }
 
 
@@ -64,19 +68,129 @@ public class LinkedList {
         }
     }
 
+    public int size() {
+        return size;
+    }
+
+
+    public Integer getLast() {
+        if (isEmpty()) {
+            return null;
+        }
+        Node temp = head;
+        //perderisa nyja ka elementin next (kam ku me shku ne nyje tjeter)
+        //shko, perndryshe ke mberri ne nyjen e fundit
+        while (temp.getNext() != null) {
+            temp = temp.getNext();
+        }
+        return temp.getData();
+    }
+
+    public void removeLast() {
+        if (isEmpty()) {
+            System.out.println("Lista eshte empty!");
+            return;
+        }
+        if (size == 1) {
+            removeFirst();
+        } else {
+            Node temp = head;
+            while (temp.getNext().getNext() != null) {
+                temp = temp.getNext();
+            }
+            temp.setNext(null);
+            size--;
+        }
+    }
+
+    public void clear() {
+        head = null;
+        size = 0;
+    }
+
+    private Node findNodeByIndex(int index) {
+        if (index < 0 || index >= size) {
+            System.out.println("Index out of range!");
+            return null;
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.getNext();
+        }
+        return temp;
+    }
+
+    public void addAt(int index, int data) {
+        if (index < 0 || index > size) {
+            System.out.println("Index out of range!");
+            return;
+        }
+        if (index == 0) {
+            addFirst(data);
+        } else if (index == size) {
+            addLast(data);
+        } else {
+            Node newNode = new Node(data);
+            Node temp = findNodeByIndex(index - 1);
+            newNode.setNext(temp.getNext());
+            temp.setNext(newNode);
+            size++;
+        }
+    }
+
+    public void addAfter(int index, int data) {
+        addAt(index + 1, data);
+    }
+
+    public Integer get(int index) {
+        if (index < 0 || index > size) {
+            System.out.println("Index out of range!");
+            return null;
+        }
+
+        if (index == 0) {
+            return getFirst();
+        } else if (index == size - 1) {
+            return getLast();
+        } else {
+            Node temp = findNodeByIndex(index);
+            return temp.getData();
+        }
+    }
+
+    public Integer getAfter(int index) {
+        return get(index + 1);
+    }
+
+
+    public void removeAt(int index) {
+        if (isEmpty()) {
+            System.out.println("Lista eshte e zbrazet!");
+            return;
+        }
+        if (index < 0 || index >= size) {
+            System.out.println("Index out of range!");
+            return;
+        }
+        if (index == 0) {
+            removeFirst();
+        } else if (index == size - 1) {
+            removeLast();
+        } else {
+            Node temp = findNodeByIndex(index - 1);
+            temp.setNext(temp.getNext().getNext());
+            size--;
+        }
+    }
+
+
 
     /*
-        isEmpty()
-        size()
-        addFirst(...)
-        addLast(...)
-        addAfter(int index, int data)
-        addBefore(int index, int data)
-        getFirst()
-        getLast()
-        get(int index);
-        removeFirst();
-        removeLast();
+
+
+
+
+
         remove(int index);
         addAll(...)
         removeAll(...)
@@ -86,7 +200,6 @@ public class LinkedList {
         setFirst(int data)
         setLast(int data)
         set(int index, int data)
-        clear()
         contains(int data)
         indexOf(int data)
         indexOf(int afterIndex, int data)
