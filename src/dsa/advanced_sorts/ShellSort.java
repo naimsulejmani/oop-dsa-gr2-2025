dc8276048f1e3abbfe43bde87213e773d8d13bf6
Naim Sulejmani <naim.sulejmani@gmail.com>
2025-06-21 22:55:54 +0200

    Shell Sort

---
+++ src/dsa/advanced_sorts/ShellSort.java
@@ -0,0 +1,18 @@
+package dsa.advanced_sorts;
+
+public abstract class ShellSort {
+
+    public static void sort(int[] arr) {
+        int n = arr.length;
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i += 1) {
+                int temp = arr[i];
+                int j;
+                for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                    arr[j] = arr[j - gap];
+                }
+                arr[j] = temp;
+            }
+        }
+    }
+}
